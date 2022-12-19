0a1797ff9b90043f60d89079096c3c2765c85e23
Evelien Vanophalvens <Evelien.Vanophalvens@telenet.be>
2022-12-19 17:07:49 +0100

    first commit, api works, title on screen

---
+++ components/products.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+const ProductItem = props => {
+  return (
+    <TouchableOpacity activeOpacity={0.5}>
+      <View style={styles.listItem}>
+        <Text>{props.title}</Text>
+      </View>
+    </TouchableOpacity >
+
+  );
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    borderColor: 'grey',
+    borderStyle: 'dashed',
+    borderWidth: 0.5,
+  },
+  image:{
+    width: 100,
+    height: 100,
+  }
+});
+export default ProductItem;
---
+++ screens/OverviewScreen.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
+
+import ProductItem from '../components/products';
+
+const OverviewScreen = ({ navigation }) => {
+
+  const [products, setProducts] = useState([]); //movies read only, setMovies is een functie om de movies aan te passen 
+
+  const getProducts = async () => { //-> assincrone functie, pas na een tijd uitgevoerd, je weet wanneer, pas api wanneer app al klaar staat
+    try { //stuk code proberen als het niet lukt, error afprinten
+      const response = await fetch("https://evelienvanophalvens.be/index.php/wp-json/wp/v2/posts?categories=12&_embed", { //data ophalen, response = wat je terugkrijgr van API = resultaat, await= //wachten tot fetch klaar is, !belangrijk!
+        "method": "GET", //GET = data ophalen, POST = data verzenden
+      })
+      const json = await response.json(); //naar het  juiste bestandsformaat omzetten
+      console.log(json);
+      //movies = json.result) mag niet !!!!!!!!!
+      setProducts(json);
+    } catch (error) {
+      console.error(error);
+      
+    }
+  }
+
+  useEffect(() => {
+    getProducts();//laad upcomming movies wanneer het scherm laadt 
+  }, []);
+
+
+  return (
+    <View style={styles.screen}>
+      <FlatList //lussen, zoals foor loop
+        data={products}
+        keyExtractor={item => item.id}//gebruik imdb_id als key voor de flatlist
+        renderItem={({ item }) => (
+          <ProductItem
+            title={item.title.rendered}
+          />
+        )}
+      />
+    </View >
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  }
+});
+export default OverviewScreen;
